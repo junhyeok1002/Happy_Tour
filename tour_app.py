8c195a1ab6a6c2ba6057671e0455cdb9ca67fdbb
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2023-08-01 06:55:20 +0900

    Add files via upload

--- tour_app.py
+++ tour_app.py
@@ -136,11 +136,11 @@ custom_style = """
         .custom-ticket-font {
             font-size: 1rem;
             text-align: center;
+            letter-spacing:-0.05rem;
         }
         .custom-ticket-small-font {
             font-size: 0.7rem;
             text-align: center;
-            letter-spacing:-0.1rem;
         }
     </style>
 """
