8628eb26ea2123e5ef466c139acb6812292a6841
Jun Hyeok Choi <113586985+junhyeok1002@users.noreply.github.com>
2023-08-01 06:39:47 +0900

    Add files via upload

--- tour_app.py
+++ tour_app.py
@@ -271,7 +271,7 @@ st.markdown("""
     }
     
     td:nth-child(1) { 
-        width: 30%; 
+        width: 25%; 
         border-left: 0.3rem solid #F0A23D;
         border-right: 1px solid #ffffff; 
         border-bottom: 0.1rem solid #F0A23D;
@@ -285,7 +285,7 @@ st.markdown("""
         border-top: 0.1rem solid #F0A23D;
     }
     td:nth-child(3) { 
-        width: 30%; 
+        width: 25%; 
         border-right: 2px dashed #F0A23D;
     }
     td:nth-child(5) { 
